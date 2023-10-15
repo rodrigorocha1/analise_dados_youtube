3c7b598a9e4c3f4ad70cadb9c909eed9412dd374
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-10-15 19:28:34 -0300

    troca do noe da pasta

--- dags/dag_youtube.py
+++ dags/dag_youtube.py
@@ -53,6 +53,7 @@ with DAG(
         lista_task_historico = []
         for termo_assunto in lista_assunto:
             id_termo_assunto = termo_assunto.replace(' ', '_').lower()
+            termo_assunto_pasta = termo_assunto.replace(' ', '_')
             extracao_api_youtube_historico_pesquisa = YoutubeBuscaOperator(
                 task_id=f'id_youtube_api_historico_pesquisa_{id_termo_assunto}',
                 data_inicio=data_hora_busca,
@@ -64,14 +65,14 @@ with DAG(
                 extracao_dados=(
                     InfraJson(
                         diretorio_datalake='bronze',
-                        termo_assunto=termo_assunto.replace(' ', '_'),
+                        termo_assunto=f'assunto_{termo_assunto_pasta}',
                         path_extracao=data,
                         metrica='requisicao_busca',
                         nome_arquivo='req_busca.json'
                     ),
                     InfraPicke(
                         diretorio_datalake='bronze',
-                        termo_assunto=termo_assunto.replace(' ', '_'),
+                        termo_assunto=f'assunto_{termo_assunto_pasta}',
                         path_extracao='id_video',
                         metrica=None,
                         nome_arquivo='id_video.pkl'
@@ -86,6 +87,7 @@ with DAG(
         lista_task_dados_videos = []
         for termo_assunto in lista_assunto:
             id_termo_assunto = termo_assunto.replace(' ', '_').lower()
+            termo_assunto_pasta = termo_assunto.replace(' ', '_')
             extracao_api_youtube_dados_videos_estatistica = YoutubeBuscaVideoOperator(
                 task_id=f'id_extracao_api_youtube_dados_videos_estatistica_{id_termo_assunto}',
                 data_inicio=None,
@@ -93,7 +95,7 @@ with DAG(
                     data_inicio=data_hora_busca,
                     carregar_dados=InfraPicke(
                         diretorio_datalake='bronze',
-                        termo_assunto=termo_assunto.replace(' ', '_'),
+                        termo_assunto=f'assunto_{termo_assunto_pasta}',
                         path_extracao='id_video',
                         metrica=None,
                         nome_arquivo='id_video.pkl'
@@ -102,7 +104,7 @@ with DAG(
                 extracao_dados=None,
                 extracao_unica=InfraJson(
                     diretorio_datalake='bronze',
-                    termo_assunto=termo_assunto.replace(' ', '_'),
+                    termo_assunto=f'assunto_{termo_assunto_pasta}',
                     path_extracao=data,
                     metrica='estatisticas',
                     nome_arquivo='req_video.json'
@@ -117,6 +119,7 @@ with DAG(
         lista_task_comentarios = []
         for termo_assunto in lista_assunto:
             id_termo_assunto = termo_assunto.replace(' ', '_').lower()
+            termo_assunto_pasta = termo_assunto.replace(' ', '_')
             extracao_api_youtube_dados_videos_comentarios = YoutubeBuscaComentariosOperator(
                 task_id=f'id_extracao_comentarios_{id_termo_assunto}',
                 data_inicio=None,
@@ -124,7 +127,7 @@ with DAG(
                     data_inicio=data_hora_busca,
                     carregar_dados=InfraPicke(
                         diretorio_datalake='bronze',
-                        termo_assunto=termo_assunto.replace(' ', '_'),
+                        termo_assunto=f'assunto_{termo_assunto_pasta}',
                         path_extracao='id_video',
                         metrica=None,
                         nome_arquivo='id_video.pkl'
@@ -133,14 +136,14 @@ with DAG(
                 extracao_dados=(
                     InfraJson(
                         diretorio_datalake='bronze',
-                        termo_assunto=termo_assunto.replace(' ', '_'),
+                        termo_assunto=f'assunto_{termo_assunto_pasta}',
                         path_extracao=data,
                         metrica='comentarios',
                         nome_arquivo='req_comentarios.json'
                     ),
                     InfraPicke(
                         diretorio_datalake='bronze',
-                        termo_assunto=termo_assunto.replace(' ', '_'),
+                        termo_assunto=f'assunto_{termo_assunto_pasta}',
                         path_extracao='id_comentario',
                         metrica=None,
                         nome_arquivo='id_comentario.pkl'
@@ -153,6 +156,7 @@ with DAG(
     with TaskGroup('tsk_extracao_youtube_dados_videos_respostas', dag=dag) as tg4:
         lista_task_respostas = []
         for termo_assunto in lista_assunto:
+            termo_assunto_pasta = termo_assunto.replace(' ', '_')
             id_termo_assunto = termo_assunto.replace(' ', '_').lower()
             extracao_api_youtube_dados_videos_respostas = YoutubeBuscaRespostasOperator(
                 task_id=f'id_extracao_resposta_comentario_{id_termo_assunto}',
@@ -161,7 +165,7 @@ with DAG(
                         data_inicio=data_hora_busca,
                         carregar_dados=InfraPicke(
                             diretorio_datalake='bronze',
-                            termo_assunto=termo_assunto.replace(' ', '_'),
+                            termo_assunto=f'assunto_{termo_assunto_pasta}',
                             path_extracao='id_comentario',
                             metrica=None,
                             nome_arquivo='id_comentario.pkl'
@@ -171,7 +175,7 @@ with DAG(
                 extracao_unica=(
                     InfraJson(
                         diretorio_datalake='bronze',
-                        termo_assunto=termo_assunto.replace(' ', '_'),
+                        termo_assunto=f'assunto_{termo_assunto_pasta}',
                         path_extracao=data,
                         metrica='resposta_comentarios',
                         nome_arquivo='resposta_comentarios.json'
