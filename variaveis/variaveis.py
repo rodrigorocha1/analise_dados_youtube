2e77f3d08f4c6c83acfc790a711be9f877469d9c
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-10-15 19:02:46 -0300

    Verificação de conexão por chave

--- variaveis/variaveis.py
+++ variaveis/variaveis.py
@@ -1,5 +1,26 @@
 from airflow.models import Variable
-import os
+import requests
 
-url_youtube = Variable.get('URL_API_YOUTUBE')
-chave_youtube = Variable.get('key_youtube')
+
+def checar_url(url: str, key: str):
+    params = {
+        'part': 'snippet',
+        'regionCode': 'BR',
+        'key':  key
+
+    }
+    req = requests.get(url, params=params)
+    if req.status_code == 200:
+        return True
+    return False
+
+
+if checar_url(Variable.get('URL_API_YOUTUBE'), Variable.get('key_youtube')):
+    url_youtube = Variable.get('URL_API_YOUTUBE')
+    chave_youtube = Variable.get('key_youtube')
+elif checar_url(Variable.get('URL_API_YOUTUBE'), Variable.get('key_youtube_dois')):
+    url_youtube = Variable.get('URL_API_YOUTUBE')
+    chave_youtube = Variable.get('key_youtube_dois')
+else:
+    url_youtube = Variable.get('URL_API_YOUTUBE')
+    chave_youtube = Variable.get('key_youtube_tres')
