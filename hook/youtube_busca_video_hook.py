46db84c3dfc786c465f4262ddf4eec7cf0bcc709
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-10-16 16:53:59 -0300

    if

--- hook/youtube_busca_video_hook.py
+++ hook/youtube_busca_video_hook.py
@@ -25,6 +25,8 @@ class YoutubeBuscaVideoHook(YoutubeHook):
 
             } for id_video in lista_videos
         ]
+        print(params)
+
         response = self._executar_paginacao(
             url=url,
             session=session,
--- variaveis/variaveis.py
+++ variaveis/variaveis.py
@@ -9,18 +9,33 @@ def checar_url(url: str, key: str):
         'key':  key
 
     }
+    url = url + '/videoCategories/'
     req = requests.get(url, params=params)
+    print('status code', req.status_code)
     if req.status_code == 200:
         return True
     return False
 
 
 if checar_url(Variable.get('URL_API_YOUTUBE'), Variable.get('key_youtube')):
+    print('key_youtube')
     url_youtube = Variable.get('URL_API_YOUTUBE')
     chave_youtube = Variable.get('key_youtube')
 elif checar_url(Variable.get('URL_API_YOUTUBE'), Variable.get('key_youtube_dois')):
     url_youtube = Variable.get('URL_API_YOUTUBE')
     chave_youtube = Variable.get('key_youtube_dois')
-else:
+    print('key_youtube_dois')
+elif checar_url(Variable.get('URL_API_YOUTUBE'), Variable.get('key_youtube_tres')):
     url_youtube = Variable.get('URL_API_YOUTUBE')
     chave_youtube = Variable.get('key_youtube_tres')
+    print('key_youtube_tres')
+elif checar_url(Variable.get('URL_API_YOUTUBE'), Variable.get('key_youtube_quatro')):
+    url_youtube = Variable.get('URL_API_YOUTUBE')
+    chave_youtube = Variable.get('key_youtube_quatro')
+    print('key_youtube_quatro')
+else:
+    url_youtube = ''
+    chave_youtube = ''
+print('URL', url_youtube)
+print('KEY', chave_youtube)
+checar_url(Variable.get('URL_API_YOUTUBE'), Variable.get('key_youtube_quatro'))
