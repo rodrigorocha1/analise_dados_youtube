ec735b027820ec9964b0d5fcaf3a02f9e2ba2f2b
rodrigorocha1 <silva.rodrigo31@outlook.com>
2023-12-13 20:13:59 -0300

    método para gerar gráfico de barra

---
+++ src/visualization/visualizacao.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import plotly.express as px
+
+
+class Visualizacao:
+
+    def __init__(self, df_resultado: pd.DataFrame) -> None:
+        """Init da classe
+
+        Args:
+            df_resultado (pd.DataFrame): recebe um dataframe do pandas
+        """
+        self.__df_resultado = df_resultado
+
+    def gerar_grafico_de_barras(self, coluna_x: str, coluna_y: str, titulo):
+        # Gere documentação
+        fig = px.bar(
+            self.__df_resultado,
+            x=coluna_x,
+            y=coluna_y,
+            text_auto=True
+        )
+
+        fig.update_layout(
+            title_text=titulo,
+            showlegend=True,
+            title=dict(x=0.5, font=dict(color='white')),
+            plot_bgcolor='#1F2326',
+            yaxis=dict(visible=False),
+            margin=dict(l=20, r=20, t=40, b=20, pad=4),
+            paper_bgcolor='#1F2326',
+            xaxis=dict(title='', tickfont=dict(color='white')),
+            legend=dict(font=dict(color='white')),
+        )
+
+        fig.update_traces(
+            textfont_color='white',
+            marker_color='#FFA500',
+            textfont_size=16
+        )
+        return fig
