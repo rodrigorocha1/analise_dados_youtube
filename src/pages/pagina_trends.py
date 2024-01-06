c6070ea22d6f21571ed8e2f30201824d9b149a15
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-01-06 14:27:29 -0300

    formatações páginas

--- src/pages/pagina_trends.py
+++ src/pages/pagina_trends.py
@@ -11,22 +11,56 @@ class PaginaTrends:
         self.tela = self.__get_layout()
         self.__gerar_calbacks()
 
-    def __get_layout(self):
-        pass
+    def __gerar_layout_popularidade(self):
+        return [
+            html.P(
+                "Popularidade",
+                id="id_titulo_popularidade",
+                className="class_titulo_div_popularidede",
+            ),
+            dbc.Row(
+                [
+                    dbc.Col(html.Label("Selecione o dia"), lg=6),
+                    dbc.Col(html.Label("Selecione o dia"), lg=6),
+                ]
+            ),
+            dbc.Tabs(
+                [
+                    dbc.Tab(
+                        label="Visualizações", tab_id="tab_visualizacoes_populares"
+                    ),
+                    dbc.Tab(label="Comentários", tab_id="tab_comentarios_populares"),
+                    dbc.Tab(label="Likes", tab_id="tab_likes_populares"),
+                ]
+            ),
+            html.Div(id="id_content_desempenho"),
+        ]
 
-    def __gerar_calbacks(self):
+    def __get_layout(self):
         return html.Div(
             [
                 dbc.Row(
                     [
                         dbc.Col(
-                            [],
+                            [
+                                html.Div(
+                                    self.__gerar_layout_popularidade(),
+                                    id="id_div_top_dez_trends",
+                                    className="class_div_top_dez_trends",
+                                )
+                            ],
                             lg=6,
                             id="id_top_dez_trends",
                             className="class_top_dez_trends",
                         ),
                         dbc.Col(
-                            [],
+                            [
+                                html.Div(
+                                    [],
+                                    id="id_div_popularidade_categoria",
+                                    className="class_div_popularidade_categoria",
+                                )
+                            ],
                             id="id_popularidade_categoria",
                             className="class_popularidade_categoria",
                             lg=6,
@@ -39,6 +73,9 @@ class PaginaTrends:
             id="id_main_trends",
             className="class_name_trends",
         )
+
+    def __gerar_calbacks(self):
+        pass
 
 
 pt = PaginaTrends()
