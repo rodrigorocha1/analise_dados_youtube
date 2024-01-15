21bfc0617c43134a61c20ba675ae457349348dd0
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-01-15 20:19:32 -0300

    sete horas de extração

--- dags/dag_youtube.py
+++ dags/dag_youtube.py
@@ -25,7 +25,7 @@ import logging
 
 data_hora_atual = pendulum.now('America/Sao_Paulo').to_iso8601_string()
 data_hora_atual = pendulum.parse(data_hora_atual)
-data_hora_busca = data_hora_atual.subtract(hours=1)
+data_hora_busca = data_hora_atual.subtract(hours=7)
 data_hora_busca = data_hora_busca.strftime('%Y-%m-%dT%H:%M:%SZ')
 
 
