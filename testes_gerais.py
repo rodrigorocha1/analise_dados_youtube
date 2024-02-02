1c0870767c4c6c2be219fb3a1a1d365acaae99d2
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-02-02 20:57:25 -0300

    novos procedimentos etl

--- testes_gerais.py
+++ testes_gerais.py
@@ -6,3 +6,4 @@ logging.debug('This message should go to the log file')
 logging.info('So should this')
 logging.warning('And this, too')
 logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+
